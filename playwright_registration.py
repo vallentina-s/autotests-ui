72b8dbb7b833c93db51629764957dd1f3dbd249c
vallentina-s <vallentinasemyonova@gmail.com>
2025-06-30 16:44:36 +0500

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -2,7 +2,8 @@ from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
@@ -17,8 +18,18 @@ with sync_playwright() as playwright:
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
-    expect(page).to_have_url("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
     dashboard_title=page.get_by_test_id('dashboard-toolbar-title-text')
     expect(dashboard_title).to_be_visible()
     expect(dashboard_title).to_have_text('Dashboard')
+
+    context.storage_state(path='browser-state.json')
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+
+    page.wait_for_timeout(5000)
