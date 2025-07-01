a0be414d99fb0727abde08e82298760b8eb23656
vallentina-s <vallentinasemyonova@gmail.com>
2025-07-01 12:45:49 +0500

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,23 @@
+def test_user_login():
+    print("Hello!")
+
+
+class TestUserLogin:
+    def test_1(self):
+        ...
+
+    def test_2(self):
+        ...
+
+
+def test_assert_positie_case():
+    assert (2 + 2) == 4
+    assert (3 + 3) == 6
+    assert (4 + 4) == 8
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, "(2 +2) !=5"
+
+
+
+
