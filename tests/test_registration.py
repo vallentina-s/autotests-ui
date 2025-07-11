9d34a8878fd72126a76983eb1214339a76d7191a
vallentina-s <vallentinasemyonova@gmail.com>
2025-07-11 17:52:39 +0500

    Знакомство с PageObject. Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,24 +1,18 @@
-from playwright.sync_api import expect, Page
 import pytest
+from playwright.sync_api import Page
+
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):
-        chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+@pytest.mark.parametrize(
+    'email, login, password',[('user.name@gmail.com', 'username', 'password'),])
 
-        email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
-
-        login_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-        login_input.fill('username')
-
-        password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-        expect(dashboard_title).to_be_visible()
-        expect(dashboard_title).to_have_text('Dashboard')
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email: str, login: str, password: str):
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.fill_registration_form(email=email, login=login, password=password)
+    registration_page.click_registration_button()
+    dashboard_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    dashboard_page.check_visible_dashboard_title()
