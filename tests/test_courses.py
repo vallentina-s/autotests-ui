3dd54951fdeaf3878f5cdf96740fc82a7042d05e
vallentina-s <vallentinasemyonova@gmail.com>
2025-07-16 15:31:13 +0500

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,4 @@
 import pytest
-from playwright.sync_api import Page, expect
 
 from pages.create_course_page import CreateCoursePage
 from pages.courses_list_page import CoursesListPage
@@ -10,28 +9,24 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
     courses_list_page.sidebar.check_visible()
     courses_list_page.navbar.check_visible('username')
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
 
 def test_create_course (create_course_page: CreateCoursePage,courses_list_page: CoursesListPage):
     create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view(is_image_uploaded = False)
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded = False)
     create_course_page.check_visible_create_course_form(
-        title = '',
-        estimated_time = '',
-        description = '',
-        max_score = '0',
-        min_score = '0'
+        title = '', estimated_time = '', description = '', max_score = '0', min_score = '0'
     )
+
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image(file='./testdata/files/image.png')
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+
+    create_course_page.image_upload_widget.upload_preview_image(file='./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         title="Playwright",
         estimated_time="2 weeks",
@@ -40,12 +35,7 @@ def test_create_course (create_course_page: CreateCoursePage,courses_list_page: 
         min_score="10"
     )
     create_course_page.click_create_course_button()
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
-        index=0,
-        title = "Playwright",
-        estimated_time="2 weeks",
-        max_score="100",
-        min_score="10"
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(
+        index=0, title = "Playwright", estimated_time="2 weeks", max_score="100", min_score="10"
     )
